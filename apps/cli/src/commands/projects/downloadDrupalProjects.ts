f3c209477e751abca1899f0169bed224ec4b1182
Desoindx <xavier.desoindre@hotmail.fr>
2023-03-16 11:00:11 +0100

    feat: download drupal projects to json file

--- apps/cli/src/commands/projects/downloadDrupalProjects.ts
+++ apps/cli/src/commands/projects/downloadDrupalProjects.ts
@@ -4,19 +4,20 @@ import { Command } from '@commander-js/extra-typings'
 import { output } from '@sde/cli/output'
 import { fetchDrupalProjects } from '@sde/cli/drupal/fetchDrupalProjects'
 
-export const drupalOutputFile = resolve('var', 'drupal-projects.json')
+const outputDir = 'var'
+const outputFile = resolve(outputDir, 'drupal-projects.json')
 
 export const downloadDrupalProjects = new Command()
   .command('projects:drupal:download')
   .action(async () => {
 
     const drupalProjects = await fetchDrupalProjects()
-    if (!fs.existsSync('var')) {
-      output(`Creating var directory`)
-      fs.mkdirSync('var')
+    if (!fs.existsSync(outputDir)) {
+      output(`Creating ${outputDir} directory`)
+      fs.mkdirSync(outputDir)
     }
 
-    fs.writeFileSync(drupalOutputFile, JSON.stringify(drupalProjects))
+    fs.writeFileSync(outputFile, JSON.stringify(drupalProjects))
 
-    output(`${drupalProjects.length} projects exported to ${drupalOutputFile}`)
+    output(`${drupalProjects.length} projects exported to ${outputFile}`)
   })
