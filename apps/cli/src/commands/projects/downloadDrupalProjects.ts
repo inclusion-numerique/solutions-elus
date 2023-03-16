23d9821421480c001617c8ce05191f759e8b060d
Desoindx <xavier.desoindre@hotmail.fr>
2023-03-16 11:00:11 +0100

    feat: download drupal projects to json file

--- apps/cli/src/commands/projects/downloadDrupalProjects.ts
+++ apps/cli/src/commands/projects/downloadDrupalProjects.ts
@@ -1,14 +1,23 @@
+import fs from "node:fs"
+import { resolve } from 'node:path'
 import { Command } from '@commander-js/extra-typings'
 import { output } from '@sde/cli/output'
 import { fetchDrupalProjects } from '@sde/cli/drupal/fetchDrupalProjects'
+
+const outputDir = 'var'
+const outputFile = resolve(outputDir, 'drupal-projects.json')
 
 export const downloadDrupalProjects = new Command()
   .command('projects:drupal:download')
   .action(async () => {
 
     const drupalProjects = await fetchDrupalProjects()
+    if (!fs.existsSync(outputDir)) {
+      output(`Creating ${outputDir} directory`)
+      fs.mkdirSync(outputDir)
+    }
 
-    // TODO Write all this to a json for usage in another cli to import/merge to Grist
+    fs.writeFileSync(outputFile, JSON.stringify(drupalProjects))
 
-    output(`${drupalProjects.length} projects found`)
+    output(`${drupalProjects.length} projects exported to ${outputFile}`)
   })
