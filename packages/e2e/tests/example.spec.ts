72a102c886de636edd17c3bfb4fa07536848a5ff
Hugues Maignol <hugues@kime.tech>
2023-03-18 19:33:58 +0100

    ci: e2e setup

--- packages/e2e/tests/example.spec.ts
+++ packages/e2e/tests/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test'
 
 test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+  await page.goto('http://localhost:3000/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+  await expect(page).toHaveTitle(/Solutions d'élus/)
 
   // create a locator
   const getStarted = page.getByRole('link', { name: 'Get started' })
