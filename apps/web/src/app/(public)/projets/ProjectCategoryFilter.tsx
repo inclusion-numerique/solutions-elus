b0bf677ac99704258ac2eeb9a7e1faaaf3e738c1
Hugues Maignol <hugues@kime.tech>
2023-03-20 12:37:30 +0100

    feat: select with tags component for mobile filtering

---
+++ apps/web/src/app/(public)/projets/ProjectCategoryFilter.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { categories } from '@sde/web/anctProjects'
+import { ProjectFilterResetButton } from '@sde/web/app/(public)/projets/ProjectFilterResetButton'
+import { useCategoriesFilters } from '@sde/web/legacyProject/projectFiltersStore'
+import { SelectTagsFormField } from '@sde/web/app/(public)/projets/SelectWithTagsFilter'
+
+export const ProjectCategoryFilter = () => {
+  const selectedCategories = useCategoriesFilters(({ selected }) => selected)
+  const toggleCategory = useCategoriesFilters(({ toggle }) => toggle)
+  const reset = useCategoriesFilters(({ reset }) => reset)
+  const isEmpty = useCategoriesFilters(({ selected }) => selected.size === 0)
+
+  return (
+    <>
+      <div className="fr-hidden-md fr-px-2w fr-px-md-4w">
+        <SelectTagsFormField
+          id="population-brackets"
+          value={selectedCategories}
+          label="Par thématiques"
+          onAdd={toggleCategory}
+          onRemove={toggleCategory}
+          options={categories}
+        />
+      </div>
+      <div className="fr-hidden fr-unhidden-md fr-px-2w fr-px-md-4w">
+        <p className="fr-text--regular fr-text--bold fr-text--lg fr-mt-8v fr-mb-2v">
+          Rechercher par thématiques
+        </p>
+        {categories.map((category) => {
+          const isSelected = selectedCategories.has(category)
+          return (
+            <button
+              type="button"
+              key={category}
+              className={`fr-tag fr-mt-2v fr-mr-2v `}
+              onClick={() => toggleCategory(category)}
+              aria-pressed={isSelected ? 'true' : 'false'}
+              aria-label={`Retirer ${category}`}
+            >
+              {category}
+            </button>
+          )
+        })}
+        <br />
+        <div className="fr-mt-4v">
+          <ProjectFilterResetButton
+            label="Voir toutes les thématiques"
+            onClick={reset}
+            hidden={isEmpty}
+          />
+        </div>
+      </div>
+    </>
+  )
+}
