cd6ccee8eddf642ee520003703b85440a3fec9ab
Hugues Maignol <hugues@kime.tech>
2023-03-22 11:44:49 +0100

    feat: order projects by created desc

--- apps/web/src/legacyProject/projectsList.ts
+++ apps/web/src/legacyProject/projectsList.ts
@@ -28,6 +28,9 @@ export const getProjectsList = () =>
       coverImageAlt: true,
       slug: true,
     },
+    orderBy: {
+      created: 'desc',
+    },
   })
 
 export const getProject = (slug: string) =>
