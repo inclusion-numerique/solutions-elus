717ca3e098676b66bd1d266865aa49ec1a4822eb
Hugues Maignol <hugues@kime.tech>
2023-03-23 18:52:49 +0100

    feat: better health check

--- apps/web/src/pages/api/health.ts
+++ apps/web/src/pages/api/health.ts
@@ -1,5 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { PublicWebAppConfig } from '@sde/web/webAppConfig'
+import { prismaClient } from '@sde/web/prismaClient'
 
-export default function health(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ status: 'ok' })
+const databaseStatus = () =>
+  prismaClient.$queryRaw`SELECT 1`
+    .then(() => ({ status: 'ok' }))
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    .catch((error) => ({ status: 'error', error }))
+
+const sentryStatus = PublicWebAppConfig.Sentry.dsn
+  ? {
+      enabled: true,
+      environment: PublicWebAppConfig.Sentry.environment,
+    }
+  : { enabled: false }
+
+export default async function health(
+  request: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const database = await databaseStatus()
+  const { status } = database
+  const { headers } = request
+  const { host } = request.headers
+  const containerImage = process.env.MSS_WEB_IMAGE
+
+  res.status(status === 'ok' ? 200 : 503).json({
+    status,
+    database,
+    headers,
+    host,
+    containerImage,
+    sentry: sentryStatus,
+  })
 }
