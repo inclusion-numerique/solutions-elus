217c2381afa3717e758f175c46c87cd7c8424e5b
Hugues Maignol <hugues@kime.tech>
2023-03-28 13:14:36 +0200

    fix: timestamp test dumbs

--- apps/cli/src/drupal/convertDrupalProjectsToGristProjects.ts
+++ apps/cli/src/drupal/convertDrupalProjectsToGristProjects.ts
@@ -22,7 +22,7 @@ export const convertToGristProject = (
   Programme: 0,
   Localisation: 0,
   Population: 0,
-  A_publier_le: Date.now() / 1000,
+  A_publier_le: new Date('2023-01-01').getTime() / 1000,
 })
 
 export const convertDrupalProjectsToGristProjects = (
