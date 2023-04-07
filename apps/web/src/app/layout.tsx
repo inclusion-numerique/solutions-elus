3e21de23aa617d861d466f916b4b94370bd08c5e
Hugues Maignol <hugues@kime.tech>
2023-04-07 15:32:40 +0200

    feat: removed head.tsx api from next 13.3 -> metadata

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,14 +1,52 @@
 import '@sde/web/app/app.css'
 import { PropsWithChildren } from 'react'
-import { EnvInformation } from './EnvInformation'
+import { PublicWebAppConfig, ServerWebAppConfig } from '@sde/web/webAppConfig'
+import { Metadata } from 'next'
+import { Dsfr } from '@sde/web/app/Dsfr'
+import { Matomo } from '@sde/web/app/Matomo'
+import { PreloadResources } from '@sde/web/app/PreloadResources'
+import { EnvInformation } from '@sde/web/app/EnvInformation'
+import { getServerBaseUrl } from '@sde/web/utils/baseUrl'
 
-const RootLayout = ({ children }: PropsWithChildren) => (
-  <html lang="fr" data-fr-theme="light" data-fr-scheme="light">
-    <body>
-      <EnvInformation />
-      {children}
-    </body>
-  </html>
-)
+export const generateMetadata = (): Metadata => {
+  const mainOrPreviewMeta: Metadata = ServerWebAppConfig.isMain
+    ? {}
+    : { robots: 'noindex' }
+
+  return {
+    metadataBase: new URL(getServerBaseUrl()),
+    title: PublicWebAppConfig.projectTitle,
+    themeColor: '#000091',
+    icons: {
+      icon: '/favicon.ico',
+      shortcut: '/favicon/favicon.ico',
+      apple: '/favicon/apple-touch-icon.png',
+      other: {
+        rel: 'icon',
+        url: '/favicon/favicon.svg',
+        type: 'image/svg+xml',
+      },
+    },
+    description: PublicWebAppConfig.projectTitle,
+    manifest: '/favicon/manifest.webmanifest',
+  }
+}
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  // Do we want to disable SSG for CSFR on this website ?
+  // const nonce = headers().get('x-sde-script-nonce') ?? undefined
+  const nonce = undefined
+  return (
+    <html lang="fr" data-fr-theme="light" data-fr-scheme="light">
+      <body>
+        <PreloadResources />
+        <Dsfr nonce={nonce} />
+        <Matomo nonce={nonce} />
+        <EnvInformation />
+        {children}
+      </body>
+    </html>
+  )
+}
 
 export default RootLayout
