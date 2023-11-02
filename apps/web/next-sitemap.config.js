6ce9b60250530cf99bd30400f73ac4c0d421e18e
aurelientanguy <aurelientanguy@github.com>
2023-11-02 16:25:24 +0100

    fix: revert next-sitemap.config.js to static siteUrl

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 
-import { getServerUrl } from '@sde/web/utils/baseUrl';
-
 module.exports = {
-  // siteUrl: 'https://solutionsdelus.gouv.fr',
-  siteUrl: getServerUrl(),
+  siteUrl: 'https://solutionsdelus.gouv.fr',
   generateRobotsTxt: true,
 };
