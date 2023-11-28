1ef771754baf1243887103684ac10d49b6fa4daf
aurelientanguy <aurelien.tanguy1@gmail.com>
2023-11-28 21:07:52 +0100

    fix: add `seadform.net` to cors csp

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -5,17 +5,17 @@ const nodeEnvironment = process.env.NODE_ENV
 const isProd = nodeEnvironment === 'production'
 
 const contentSecurityPolicy = `
-  default-src 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
+  default-src 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com https://*.seadform.net;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com https://*.seadform.net;
   script-src-attr 'none';
   style-src 'self' https: 'unsafe-inline';
-  img-src 'self' data: https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
-  frame-src https://www.youtube-nocookie.com/ https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
+  img-src 'self' data: https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com https://*.seadform.net;
+  frame-src https://www.youtube-nocookie.com/ https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com https://*.seadform.net;
   object-src 'none';
-  connect-src 'self' https://${ServerWebAppConfig.S3.documentsBucket}.${ServerWebAppConfig.S3.host} https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
+  connect-src 'self' https://${ServerWebAppConfig.S3.documentsBucket}.${ServerWebAppConfig.S3.host} https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com https://*.seadform.net;
   worker-src 'self' blob:;
   font-src 'self' https: data:;
-  frame-ancestors 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
+  frame-ancestors 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com https://*.seadform.net;
   form-action 'self';
   base-uri 'self';
   ${isProd ? 'upgrade-insecure-requests;' : ''}
