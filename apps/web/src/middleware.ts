038b65c1e1e210ab6cfbdcd46aa209de7614cd3e
aurelientanguy <aurelien.tanguy1@gmail.com>
2023-11-28 20:56:42 +0100

    fix: update `middleware.ts` cors policy

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -5,19 +5,17 @@ const nodeEnvironment = process.env.NODE_ENV
 const isProd = nodeEnvironment === 'production'
 
 const contentSecurityPolicy = `
-  default-src 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net;
-  script-src 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net 'unsafe-inline' 'unsafe-eval';
+  default-src 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
   script-src-attr 'none';
   style-src 'self' https: 'unsafe-inline';
-  img-src 'self' data:;
-  frame-src https://www.youtube-nocookie.com/ https://*.adform.net;
+  img-src 'self' data: https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
+  frame-src https://www.youtube-nocookie.com/ https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
   object-src 'none';
-  connect-src 'self' https://${ServerWebAppConfig.S3.documentsBucket}.${ServerWebAppConfig.S3.host} 
-  https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net 
-  https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr;
+  connect-src 'self' https://${ServerWebAppConfig.S3.documentsBucket}.${ServerWebAppConfig.S3.host} https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://sentry.incubateur.net https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
   worker-src 'self' blob:;
   font-src 'self' https: data:;
-  frame-ancestors 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net;
+  frame-ancestors 'self' https://matomo.incubateur.anct.gouv.fr https://*.adform.net https://secure.adnxs.com https://www.googletagmanager.com https://*.doubleclick.net https://snap.licdn.com https://px.ads.linkedin.com;
   form-action 'self';
   base-uri 'self';
   ${isProd ? 'upgrade-insecure-requests;' : ''}
