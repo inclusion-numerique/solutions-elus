08904d39a1f917bb7f53b3213a24e602be2292a9
aurelientanguy <aurelien.tanguy1@gmail.com>
2023-12-01 16:26:17 +0100

    feat: GTM `num` param = unique int for each call

--- apps/web/src/components/scripts/GoogleTagManager.tsx
+++ apps/web/src/components/scripts/GoogleTagManager.tsx
@@ -1,5 +1,11 @@
 import Script from 'next/script'
 
+const getNum = () => {
+  const min = Math.ceil(1e14);
+  const max = Math.floor(1e15 - 1);
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 const GoogleTagManager = () => {
   const isProd = process.env.NODE_ENV === 'production'
 
@@ -14,7 +20,7 @@ const GoogleTagManager = () => {
       <Script async id="google-tag-manager" src="https://www.googletagmanager.com/gtag/js?id=DC-3689183" strategy="afterInteractive"/>
       <Script id="googe-analytics" src="/scripts/google-analytics.min.js" strategy="afterInteractive"/>
       {/* eslint-disable-next-line @next/next/no-img-element, no-template-curly-in-string */}
-      <img src="https://ad.doubleclick.net/ddm/activity/src=3689183;type=solelus;cat=2023-0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=1?" width="1" height="1" alt=""/>
+      <img src={"https://ad.doubleclick.net/ddm/activity/src=3689183;type=solelus;cat=2023-0;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;npa=;gdpr=${GDPR};gdpr_consent=${GDPR_CONSENT_755};ord=1;num=" + getNum()} width="1" height="1" alt=""/>
     </>
   );
 }
