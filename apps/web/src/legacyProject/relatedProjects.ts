b0ed36c1b16533a65051cf4cc68cea93f4cf5f89
aurelientanguy <aurelien.tanguy1@gmail.com>
2023-12-18 15:26:57 +0100

    feat: add related projects

---
+++ apps/web/src/legacyProject/relatedProjects.ts
@@ -0,0 +1,77 @@
+import { prismaClient } from '@sde/web/prismaClient'
+import { ProjectItem } from './projectsList'
+
+const searchParser = (search: string) => {
+  const terms = search.split(' ')
+  const filtered = terms.filter((term) => term.length > 5)
+  const sorted = filtered.sort((a, b) => b.length - a.length)
+  const encoded = sorted.map((term) => encodeURIComponent(term))
+  return encoded.join(' | ')
+};
+
+export const getRelatedProjects = (project: ProjectItem) =>
+  prismaClient.project.findMany({
+    take: 6,
+    where: {
+      slug: {
+        not: project.slug
+      },
+      AND: [
+        {
+          title: {
+            search: searchParser(project.title),
+            mode: 'insensitive'
+          },
+        },
+        {
+          OR: [
+            {
+              categories: {
+                hasSome: project.categories,
+              },
+            },
+            // {
+            //   program: {
+            //     name: {
+            //       startsWith: project.program?.name || "",
+            //     }
+            //   }
+            // },
+          ]
+        }
+      ],
+    },
+    select: {
+      id: true,
+      title: true,
+      localization: {
+        select: {
+          label: true,
+          department: true,
+          echelon: true,
+        },
+      },
+      slug: true,
+      coverImage: true,
+      coverImageAlt: true,
+    },
+    orderBy: [
+      {
+        _relevance: {
+          fields: ['title'],
+          search: searchParser(project.title),
+          sort: 'desc',
+        }
+      },
+      // {
+      //   published: 'desc',
+      // },
+      // {
+      //   created: 'desc',
+      // },
+    ],
+  })
+
+export type ShowcaseProject = Awaited<
+  ReturnType<typeof getRelatedProjects>
+>[number]
