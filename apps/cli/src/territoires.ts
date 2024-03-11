105fd6076bbfb043dbf1dce58f118ec5d4b220bd
aurelientanguy <aurelien.tanguy1@gmail.com>
2024-03-11 11:32:53 +0100

    fix: wrong header

--- apps/cli/src/territoires.ts
+++ apps/cli/src/territoires.ts
@@ -5,10 +5,9 @@ const authToken = process.env.API_TERRITOIRES_AUTH_TOKEN ?? ''
 // Get new JWT during build for the next 24 hours
 // https://aides-territoires.beta.gouv.fr/api
 export const getTerritoiresJWT = async () => {
-  const response = await axios.get<{ token: string }>('https://aides-territoires.beta.gouv.fr/api/connexion', {
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
+  const response = await axios.get<{ token: string }>(
+    'https://aides-territoires.beta.gouv.fr/api/connexion', {
+      headers: { 'X-Auth-Token': authToken },
   })
 
   return response.data.token
